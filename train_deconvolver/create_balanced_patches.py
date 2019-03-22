1448d19846865002a7596dd9d4f2d272a6b46359
leonardo <leonardo.scommegna@stud.unifi.it>
2019-03-22 16:14:00 +0100

    quasi completaa la creazione di patch tramite csv

--- train_deconvolver/create_balanced_patches.py
+++ train_deconvolver/create_balanced_patches.py
@@ -2,30 +2,7 @@ import os
 import pandas as pd
 import numpy as np
 import torch
-import tifffile
 
-
-def patch_generator_old(img, patch_size, perc=0.5):
-    '''extract patches from an entire substack
-    with an overlap of at most 50%'''
-    overlap_percentage = (patch_size**3) * 0.5
-    mask = np.zeros(img.shape)
-    xs, ys, zs = np.where(mask == 0)
-    points = sorted(zip(xs, ys, zs))
-    np.random.shuffle(points)
-
-    for x, y, z in points:
-        patch_mask = mask[x:x+patch_size,
-                          y:y+patch_size,
-                          z:z+patch_size]
-        overlap_area = np.sum(patch_mask)
-
-        if overlap_area < overlap_percentage and (condition1 or condition2):
-            mask[x:x+patch_size, y:y+patch_size, z:z+patch_size] = 1
-
-            yield img[x:x+patch_size,
-                      y:y+patch_size,
-                      z:z+patch_size]
 
 def read_single_csv(csv_img_path):
     df = pd.read_csv(csv_img_path, skipinitialspace=True, na_filter=False)
@@ -41,47 +18,76 @@ def read_single_csv(csv_img_path):
     return centers
 
 
-def contains_centers(coordinates, csv_img_path, patch_size, min_centers): #todo da chiarire il concetto di "contiene il centro" : almeno un pixel della palla \\
-                                                                          # di raggio 5 oppure il centro del gt (per ora la seconda)
+def contains_centers(coordinates, centers, patch_size,
+                     min_centers=1, padding=5):
     coordinates = np.array(coordinates)
-    centers = read_single_csv(csv_img_path)
-    n_centers_contained =0
+    n_centers_contained = 0
     for center in centers:
-        contained = (coordinates<center).all() and ((coordinates + patch_size ) > center).all()
+        contained = (((coordinates + padding) < center).all() and
+                     (((coordinates - padding) + patch_size) > center).all())
         if contained:
-            n_centers_contained+=1
+            n_centers_contained += 1
             if n_centers_contained >= min_centers:
                 return True
     return False
 
-def patch_generator(img_name, patch_size, gt_dir, csv_dir, perc=0.5):
+
+def not_too_dark(coordinates, patch_size, original_image, threshold):
+
+    average_gray_level = (np.mean(
+        original_image[coordinates[0]:coordinates[0]
+                       + patch_size, coordinates[1]:coordinates[1]
+                       + patch_size, coordinates[2]:coordinates[2]
+                       + patch_size]))
+
+    if average_gray_level > threshold:
+        return True
+    else:
+        return False
+
+
+counter_graylevel = 0
+
+
+def patch_generator(img_name, patch_size, img_dir, csv_dir, th=8,
+                    perc=0.5, n_centers=1):
     '''extract patches from an entire substack
     without an overlap of at most 50%'''
-    overlap_percentage = (patch_size**3) * 0.5
+    overlap_percentage = (patch_size**3) * perc
 
-    gt_img_path = os.path.join(gt_dir, img_name) +".pth"
-    csv_img_path = os.path.join(csv_dir,img_name)+"-GT.marker"
-    torch_gt_image = torch.load(gt_img_path)
-    gt_image = torch_gt_image.numpy()
+    img_path = os.path.join(img_dir, img_name) + ".pth"
+    csv_img_path = os.path.join(csv_dir, img_name) + "-GT.marker"
+    torch_image = torch.load(img_path)
+    original_image = torch_image.numpy()
 
-    mask = np.zeros(gt_image.shape)
+    mask = np.zeros(original_image.shape)
+
+    coord_list = []
+
+    centers = read_single_csv(csv_img_path)
 
     xs, ys, zs = np.where(mask == 0)
     points = sorted(zip(xs, ys, zs))
     np.random.shuffle(points)
 
-    for x, y, z in points: #lento provare tutti i punti, possibilta' di introdurre un'altra condiione di arresto
+    for x, y, z in points:  # lento provare tutti i punti, possibilta' di
+                            # introdurre un'altra condiione di arresto
         patch_mask = mask[x:x+patch_size,
                           y:y+patch_size,
                           z:z+patch_size]
         overlap_area = np.sum(patch_mask)
-        contains_centers = contains_centers([x,y,z], csv_img_path, patch_size,1 )
-        if overlap_area < overlap_percentage and (contains_centers or condition2):
+
+        contains_cells = contains_centers([x, y, z],
+                                          centers, patch_size, n_centers)
+        acceptable_graylevel = not_too_dark([x, y, z],
+                                            patch_size, original_image, th)
+        counter_graylevel += 1 if acceptable_graylevel and not contains_cells and overlap_area < overlap_percentage else 0
+        if overlap_area < overlap_percentage and (contains_cells
+                                                  or acceptable_graylevel):
             mask[x:x+patch_size, y:y+patch_size, z:z+patch_size] = 1
 
-            yield img[x:x+patch_size,
-                      y:y+patch_size,
-                      z:z+patch_size]
+            coord_list.append(x, y, z, img_name)
+    return pd.DataFrame(coord_list, columns=['x', 'y', 'z', 'img_name'])
 
 
 def patch_balancer(gt_dir, image_dir, csv_dir, target_root_dir, patch_size):
@@ -89,21 +95,15 @@ def patch_balancer(gt_dir, image_dir, csv_dir, target_root_dir, patch_size):
     patch in a balanced fashion'''
 
     img_names = [name.split(".")[0] for name in os.listdir(image_dir)]
+    df = pd.DataFrame(columns=['x', 'y', 'z', 'img_name'])
 
-    for img_name in img_names:
-        patch_generator(img_name, patch_size, gt_dir, csv_dir, 0.5 )
+    for img_name in img_names:  # XXX in lettura pd scrivere ignore_index=True
+        df.append(
+            patch_generator(img_name, patch_size, gt_dir, csv_dir, 0.5), ignore_index=True)
 
-    #todo mancano i contatori per le condizioni
-    ########## old
-    '''
-    pth_imgs = [os.path.join(gt_dir, pth_img) for pth_img in os.listdir(gt_dir)
-                if pth_img.split(".")[-1] == "pth"]
-    for img_path in pth_imgs:
-        print img_path.split('/')[-1]
-        img = torch.load(img_path)
-        for patch in patch_generator(img, patch_size):
-            print 'magi'
-    '''
+    path_save_csv = os.path.join(targer_dir, "patches.csv")
+    df.to_csv(path_save_csv)
+
 
 if __name__ == "__main__":
     gt_dir = '/home/leonardo/workspaces/bcfind/dataset/GT/3d_gt'
