62eb161f302c04f05c398820e19055072dac8969
leonardo <leonardo.scommegna@stud.unifi.it>
2019-03-23 08:37:58 +0100

    data reader testato un minimo e patch balancer funzionante ma c'è da scegliere un creiterio di arresto

--- train_deconvolver/create_balanced_patches.py
+++ train_deconvolver/create_balanced_patches.py
@@ -4,7 +4,6 @@ import numpy as np
 import torch
 
 
-
 def read_single_csv(csv_img_path):
     df = pd.read_csv(csv_img_path, skipinitialspace=True, na_filter=False)
     if '#x' in df.keys():  # fix some Vaa3d garbage
@@ -51,7 +50,7 @@ counter_graylevel = 0
 
 
 def patch_generator(img_name, patch_size, img_dir, csv_dir, th=8,
-                    perc=0.5, n_centers=1, counter_graylevel = None):
+                    perc=0.5, n_centers=1):
     '''extract patches from an entire substack
     without an overlap of at most 50%'''
     overlap_percentage = (patch_size**3) * perc
@@ -91,26 +90,23 @@ def patch_generator(img_name, patch_size, img_dir, csv_dir, th=8,
     return pd.DataFrame(coord_list, columns=['x', 'y', 'z', 'img_name'])
 
 
-def patch_balancer(image_dir, csv_dir, target_root_dir, patch_size):
+def patch_balancer(gt_dir, image_dir, csv_dir, target_root_dir, patch_size):
     '''Starting from the whole set of substacks it creates a dataset of
     patch in a balanced fashion'''
 
-    img_names = [name.split(".")[0] for name in os.listdir(image_dir)  if name.split(".")[-1] == 'pth']
-    print(img_names)
+    img_names = [name.split(".")[0] for name in os.listdir(image_dir)]
     df = pd.DataFrame(columns=['x', 'y', 'z', 'img_name'])
 
     for img_name in img_names:  # XXX in lettura pd scrivere ignore_index=True
         df.append(
-            patch_generator(img_name, patch_size, image_dir, csv_dir, 0.5), ignore_index=True)
+            patch_generator(img_name, patch_size, gt_dir, csv_dir, 0.5), ignore_index=True)
 
     path_save_csv = os.path.join(targer_dir, "patches.csv")
     df.to_csv(path_save_csv)
 
 
 if __name__ == "__main__":
-    image_dir = '/home/cosimo/machine_learning_dataset/3DImages'
+    gt_dir = '/home/leonardo/workspaces/bcfind/dataset/GT/3d_gt'
     targer_dir = ''
-    counter_graylevel =0
     patch_size = 64
-    patch_balancer(image_dir, targer_dir, patch_size = patch_size, counter_graylevel = counter_graylevel)
-    print counter_graylevel
+    patch_balancer(gt_dir, targer_dir, patch_size=patch_size)
--- train_deconvolver/data_reader.py
+++ train_deconvolver/data_reader.py
@@ -2,15 +2,51 @@ import os
 import pickle
 from torch.utils.data import Dataset
 import torch
+import pandas as pd
 # from PIL import Image
 # from torchvision import transforms
 
 
-class DataReaderWIP(Dataset):
+class DataReader_new(Dataset):
     """Documentation for DataReader
-    TODO: c'è da mettere un altro path in modo che
-    restituisca sia l'immagine di train che il gt
+    Use a csv to select patch from entire substacks
+    both from original and gt ones
     """
+    def __init__(self, img_dir, gt_dir, csv_path, transform=None):
+        super(DataReader_new, self).__init__()
+        self.img_dir = img_dir
+        self.gt_dir = gt_dir
+        self.patch_df = pd.read_csv(csv_path, comment="#",
+                                    index_col=0, dtype={"img_name": str})
+        self.transforms = transform
+        self.patch_size = int(str(pd.read_csv(csv_path, nrows=1, header=None).
+                                  take([0])).split("=")[-1])  # workaround
+        print self.patch_size
+
+    def __len__(self):
+        return self.patch_df.shape[0]
+
+    def __getitem__(self, idx):
+        x, y, z, img_name = self.patch_df.iloc[idx]
+        img_name = str(img_name)
+        img_path = os.path.join(self.img_dir, img_name) + ".pth"
+        gt_path = os.path.join(self.gt_dir, img_name) + "-GT.pth"
+        print img_path
+        image = torch.load(img_path)
+        gt = torch.load(gt_path)
+
+        original_patch = image[x:x + self.patch_size,
+                               y:y + self.patch_size,
+                               z:z + self.patch_size]
+        gt_patch = gt[x:x + self.patch_size,
+                      y:y + self.patch_size,
+                      z:z + self.patch_size]
+
+        return original_patch, gt_patch
+
+
+class DataReaderWIP(Dataset):
+
     def __init__(self, root_path, transform=None):
         super(DataReaderWIP, self).__init__()
         self.root_dir = root_path
@@ -50,3 +86,12 @@ dummy datareader
         patch = torch.random(13, 13, 13)
         gt_patch = torch.random(13, 13, 13)
         return patch, gt_patch
+
+
+if __name__ == "__main__":
+    csv_path = "/home/leonardo/Desktop/magi.csv"
+    orig_path = "/home/leonardo/workspaces/bcfind/dataset/3d_img"
+    gt_path = "/home/leonardo/workspaces/bcfind/dataset/GT/3d_gt"
+    data_reader = DataReader_new(orig_path, gt_path, csv_path)
+    train, gt = data_reader[0]
+    print train
