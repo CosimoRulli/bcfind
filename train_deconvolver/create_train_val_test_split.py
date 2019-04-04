79a1dee741a7da36299b4cd0c63ff142b91e033f
cosimo <cosimo.rulli@gmail.com>
2019-04-04 15:58:50 +0200

    Aggiunto script per splitting train_test_val

--- train_deconvolver/create_train_val_test_split.py
+++ train_deconvolver/create_train_val_test_split.py
@@ -1,9 +1,12 @@
+
+
 import os
 import pandas as pd
 import numpy as np
 import torch
 import argparse
-
+import math
+np.random.seed(41)
 
 def read_single_csv(csv_img_path):
     df = pd.read_csv(csv_img_path, skipinitialspace=True, na_filter=False)
@@ -103,32 +106,77 @@ def patch_generator(img_name, patch_size, img_dir, csv_dir, th=8,
     return pd.DataFrame(coord_list, columns=['x', 'y', 'z', 'img_name'])
 
 
-def patch_balancer(image_dir, csv_dir,target_dir, patch_size, threshold):
-    '''Starting from the whole set of substacks it creates a dataset of
-    patch in a balanced fashion'''
 
-    img_names = [name.split(".")[0] for name in os.listdir(image_dir)  if name.split(".")[-1] == 'pth']
-    print(img_names)
-    df = pd.DataFrame(columns=['x', 'y', 'z', 'img_name'])
-    for img_name in img_names:  # XXX in lettura pd scrivere ignore_index=True
-        print img_name
-        res = patch_generator(img_name, patch_size, image_dir, csv_dir, th=threshold)
+def divide_im_names(im_names, test_perc):
 
-        df=df.append(
+    n_images = int(im_names.shape[0] )# 66
+    n_test_im = int(math.floor(n_images * test_perc))
+    n_train_im = n_images - n_test_im
+    print n_test_im
+    train_indexes = [i for i in range( n_images)]
+    test_indexes = np.random.choice(train_indexes, size = n_test_im, replace=False)
+
+    #test_indexes = np.random.randint(0, n_images, size=n_test_im)
+    test_indexes.sort()
+    print  test_indexes
+    train_indexes = np.array(list(set(train_indexes) - set(test_indexes)))
+    print train_indexes
+    print len(train_indexes)
+    train_images = im_names[train_indexes]
+    test_images = im_names[test_indexes]
+    return train_images, test_images
+
+
+
+def generate_csvs(image_dir, csv_dir, target_dir, train_csv_name, val_test_csv_name,  perc_test, perc_val, patch_size, threshold):
+
+    image_names = os.listdir(image_dir)
+    print 'Total images: ' + str(len(image_names))
+    image_names = np.array([image.split('.')[0] for image in image_names])
+
+    print 'Train-test split'
+    train_images, test_images = divide_im_names(image_names, perc_test)
+
+    print 'Train-val split'
+    train_images, val_images = divide_im_names(train_images, perc_val)
+
+    print 'Train images: ' + str(train_images.shape[0])
+    print 'Validation images: '+str(len(val_images))
+    print 'Test images: ' + str(len(test_images))
+
+    print 'Generating Train csv'
+    df_train = pd.DataFrame(columns=['x', 'y', 'z', 'img_name'])
+    for train_im in train_images:
+        print train_im
+        res = patch_generator(train_im, patch_size, image_dir, csv_dir, th=threshold)
+
+        df_train = df_train.append(
             res, ignore_index=True)
 
-    path_save_csv = os.path.join(target_dir, args.name_file + "_patches.csv")
+    path_save_csv_train = os.path.join(target_dir, train_csv_name + "_patches.csv")
 
-    file = open(path_save_csv, 'a')
-    comment = '# size='+ str(patch_size) + '\n'
+    file = open(path_save_csv_train, 'a')
+    comment = '# size=' + str(patch_size) + '\n'
     file.write(comment)
-    df.to_csv(file)
+    df_train.to_csv(file)
     file.close()
 
 
+    df_val_test = pd.DataFrame(columns=['name', 'split'])
+    val_csv = [[val_name, 'VAL'] for val_name in val_images]
+    df_val = pd.DataFrame(val_csv, columns=['name', 'split'])
+    df_val_test = df_val_test.append(df_val)
+    test_csv = [[test_name, 'TEST'] for test_name in test_images]
+    df_test = pd.DataFrame(test_csv, columns=['name', 'split'])
+    df_val_test = df_val_test.append(df_test)
+    path_save_csv_val_test = os.path.join(target_dir, val_test_csv_name)
+    df_val_test.to_csv(path_save_csv_val_test)
+
+
+
+
 def get_parser():
-    parser = argparse.ArgumentParser(
-                                     formatter_class=argparse.
+    parser = argparse.ArgumentParser(formatter_class=argparse.
                                      ArgumentDefaultsHelpFormatter)
     parser.add_argument('image_dir', metavar= 'image_dir', type=str,
                         help="""Directory containing 3d images""")
@@ -139,27 +187,35 @@ def get_parser():
     parser.add_argument('target_dir', metavar='target_dir', type=str,
                         help="""Directory to put the patches.csv file in """)
 
-    parser.add_argument('patch_size', metavar='patch_size', type=int,
-                        help="""Patch dimension """)
+    parser.add_argument('train_csv_name', metavar='train_csv_name', type=str,
+                        help="""name of the csv file with train patches """)
+
+    parser.add_argument('val_test_csv_name', metavar='val_test_csv_name', type=str,
+                        help="""Name of the csv file for validation and test """)
+
+    parser.add_argument('perc_test', metavar='perc_test', type=float,
+                        help="""Test percentage in the test-train split """)
+
+    parser.add_argument('perc_val', metavar='perc_val', type=float,
+                        help="""Validation percentage in the train validation split """)
 
     parser.add_argument('threshold',  metavar='threshold', type = int,
                         default=8,
                         help=""" Threshold to dark zones selection """)
 
-    parser.add_argument('name',  metavar='name_file', type=str,
-                        help=""" name for the csv file """)
+    parser.add_argument('patch_size', metavar='patch_size', type=int,
+                        help="""Size of the cubic patches""")
+
 
     return parser
 
 
-if __name__ == "__main__":
+
+if __name__ =="__main__":
 
     parser = get_parser()
-
     args = parser.parse_args()
-    image_dir = args.image_dir
-    csv_dir = args.csv_dir
-    target_dir = args.target_dir
-    patch_size = args.patch_size
-    threshold = args.threshold
-    patch_balancer(image_dir,csv_dir, target_dir, patch_size = patch_size, threshold=threshold)
+    print args.perc_test
+    print args.perc_val
+    generate_csvs(args.image_dir, args.csv_dir, args.target_dir, args.train_csv_name, args. val_test_csv_name, args.perc_test, args.perc_val, args.patch_size,
+                  args.threshold)
